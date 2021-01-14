f38cf0a140cfde14ec086c0dee275752a968de55
mogulders <mogulders@gmail.com>
2021-01-14 12:11:42 -0600

    did mild cleanup and usability, I'm not sure how much i like the vanishing fainted and didnt faint makes clickig the button fast kind of annoying

--- src/components/userInputField.jsx
+++ src/components/userInputField.jsx
@@ -1,20 +1,32 @@
 import React from "react";
 
 export default function UserInputField(props) {
-  return (
-    <div>
-      <p>
-        Choose a move and then press battle to attack once. Move 1 has a better
-        chance of landing. Move 2 does more damage
-      </p>
-      <p>{props.userMissed ? "User Pokemon Missed" : "User Pokemon Hit"}</p>
-      <p>
-        {props.computerMissed
-          ? "Computer Pokemon Missed"
-          : "Computer Pokemon Hit"}
-      </p>
-      <p> has fainted.</p>
-      <button onClick={props.attackInOrder}>Battle</button>
-    </div>
-  );
+    return (
+        <div>
+            <p>
+                Choose a move and then press battle to attack once. Move 1 has a
+                better chance of landing. Move 2 does more damage
+            </p>
+            {props.userMissed === "false" ? null : (
+                <p>
+                    {props.userMissed
+                        ? "User Pokemon Missed"
+                        : "User Pokemon Hit"}
+                </p>
+            )}
+            {props.computerMissed === "false" ? null : (
+                <p>
+                    {props.computerMissed
+                        ? "Computer Pokemon Missed"
+                        : "Computer Pokemon Hit"}
+                </p>
+            )}
+            {props.faintedPokemon === "" ? (
+                <p></p>
+            ) : (
+                <p>{props.faintedPokemon.name} has fainted.</p>
+            )}
+            <button onClick={props.attackInOrder}>Battle</button>
+        </div>
+    );
 }
